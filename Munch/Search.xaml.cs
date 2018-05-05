839ae77a8af2ed2ad6435bcbdbe4ab03f01d8d36
Fendy <fendy@Fendys-Mac.local>
2018-05-05 09:16:21 -0700

    Base FrameWork For Munch

---
+++ Munch/Search.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Munch
+{
+    public partial class Search : ContentPage
+    {
+        public Search()
+        {
+            InitializeComponent();
+        }
+    }
+}
